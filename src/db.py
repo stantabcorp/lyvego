19bed6a8ce1f3eaf0cba768bed6d785cde43ddfc
Dylann B <31523130+takitsu21@users.noreply.github.com>
2020-08-03 17:38:11 +0200

    new annonce_notif db

--- src/db.py
+++ src/db.py
@@ -11,7 +11,7 @@ class Pool:
     async def insert(self, channel_id, message_id, streamer_id, on_end, on_change):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("INSERT INTO bot_messages(channel_id, message_id, streamer_id, on_end, on_change) VALUES(%s, %s, %s, %s, %s)", (channel_id, message_id, streamer_id, on_end, on_change, ))
                 await conn.commit()
                 await cur.close()
@@ -27,7 +27,7 @@ class Pool:
     async def clean(self, message_id):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("DELETE FROM bot_messages WHERE message_id=%s", (message_id, ))
                 await conn.commit()
                 await cur.close()
@@ -36,7 +36,7 @@ class Pool:
     async def select_message(self, streamer_id):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
-                # await conn.begin()
+
                 await cur.execute("SELECT * FROM bot_messages WHERE streamer_id=%s", (streamer_id, ))
                 r = await cur.fetchall()
                 await cur.close()
@@ -47,7 +47,7 @@ class Pool:
     async def get_topgg_user(self, id):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
-                # await conn.begin()
+
                 await cur.execute("SELECT is_accepted FROM topgg_users WHERE id=%s", (id, ))
                 r = await cur.fetchone()
                 await cur.close()
@@ -57,8 +57,33 @@ class Pool:
     async def insert_topgg(self, id, name):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("INSERT INTO topgg_users(id, name) VALUES(%s, %s)", (id, name, ))
+                await conn.commit()
+                await cur.close()
+
+    async def insert_annonce(self, owner_id, guild_id, enable):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cur:
+
+                await cur.execute("INSERT INTO annonce_notif(owner_id, guild_id, enable) VALUES(%s, %s, %s)", (owner_id, guild_id, enable, ))
+                await conn.commit()
+                await cur.close()
+
+    async def get_annonce(self, owner_id):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+
+                await cur.execute("SELECT enable FROM annonce_notif WHERE owner_id=%s", (owner_id, ))
+                r = await cur.fetchone()
+                await cur.close()
+                return r["enable"]
+
+    async def change_annonce(self, owner_id):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cur:
+
+                await cur.execute("UPDATE annonce_notif SET enable=0 WHERE owner_id=%s", (owner_id, ))
                 await conn.commit()
                 await cur.close()
 
@@ -66,7 +91,7 @@ class Pool:
     async def change_topgg(self, id):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("UPDATE topgg_users SET is_accepted=0 WHERE id=%s", (id, ))
                 await conn.commit()
                 await cur.close()
@@ -75,7 +100,7 @@ class Pool:
     async def get_guilds_registered(self):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("SELECT discord_id FROM servers")
                 r = await cur.fetchall()
                 await cur.close()
@@ -84,7 +109,7 @@ class Pool:
     async def getg_prefix(self, id):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("SELECT prefix FROM servers WHERE discord_id=%s", (id, ))
                 prefix, = await cur.fetchone()
                 await cur.close()
@@ -94,7 +119,7 @@ class Pool:
     async def getg_lang(self, id):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("SELECT lang FROM servers WHERE discord_id=%s", (id, ))
                 lang, = await cur.fetchone()
                 await cur.close()
@@ -104,7 +129,7 @@ class Pool:
     async def set_prefix(self, id, prefix):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("UPDATE servers SET prefix=%s WHERE discord_id=%s", (prefix, id, ))
                 await conn.commit()
                 await cur.close()
@@ -113,7 +138,7 @@ class Pool:
     async def set_lang(self, id, lang):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("UPDATE servers SET lang=%s WHERE discord_id=%s", (lang, id, ))
                 await conn.commit()
                 await cur.close()
@@ -122,7 +147,7 @@ class Pool:
     async def update_messages(self, id):
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                # await conn.begin()
+
                 await cur.execute("UPDATE bot_messages SET updated_at=%s WHERE streamer_id=%s AND on_change=1", (dt.datetime.now(), id, ))
                 await conn.commit()
                 await cur.close()
